c1b4fe5264be3da186aff79f1a285567789384c1
tutifruti10 <44480817+tutifruti10@users.noreply.github.com>
2018-11-13 11:42:46 +0000

    Update GP.py

--- GP.py
+++ GP.py
@@ -16,25 +16,43 @@ class GP():
 		self.Ytrain = None
 		self.trained=False
 		self.L_trained = None
-		self.params=None
+		self.par=None
 		self.bounds=None
 		
 	def add_training(self,Xtrain,Ytrain,par,bounds):
 		self.Xtrain = Xtrain
 		self.Ytrain=Ytrain
 		self.trained=True
-		self.par=params
-		theta_in=self.params[0]
+		self.par=par
+		theta_in=self.par[0]
 		self.bounds=bounds
-		theta_opt, func_min, convergence_dict = fmin_l_bfgs_b(self.lml,x0=0.1, approx_grad=True,bounds=self.bounds)
-		lml_values = list(map(itemgetter(1),theta_opt))
-		opt= theta_opt[np.argmin(lml_values)]
-		return opt
+		def lml(theta):
+				sig=1
+				par=np.array((theta,sig))
+				K=self.kernel(Xtrain,Xtrain,*par)
+				L=np.linalg.cholesky(K + 1e-6*np.eye(self.Xtrain.shape[0]))
+				log_like=-0.5*np.dot(np.linalg.solve(L,self.Ytrain).T,np.linalg.solve(L,self.Ytrain))-np.sum(np.log(np.diagonal(L)),axis=0)-(self.Xtrain.shape[0]/2)*np.log(2*np.pi)
+				print('log_like',-log_like)
+				return -log_like
+		optima= [fmin_l_bfgs_b(lml,x0=theta_in, approx_grad=True,bounds=self.bounds)]
+		bounds=np.array(self.bounds)
+		for iteration in range(self.n_rest_opt):
+                    theta_initial =\
+                        np.random.uniform(bounds[:,0],bounds[:,1])
+                    optima.append(fmin_l_bfgs_b(lml,x0=theta_in, approx_grad=True,bounds=self.bounds))
+                    print('Theta=',theta_initial )
+		lml_values = list(map(itemgetter(1),optima))
+		opt= optima[np.argmin(lml_values)][0]
+		self.par[0]= opt
+		self.K = self.kernel(Xtrain,Xtrain,*self.par)
+		L = np.linalg.cholesky(self.K + 1e-6*np.eye(Xtrain.shape[0]))
+		self.L_trained = L
+		return L
           #  self.log_marginal_likelihood_value_ = -np.min(lml_values)
 
-	def predict(self,Xpredict,params,plot=True, n=3):
+	def predict(self,Xpredict,par,plot=True, n=3):
 		kernel_ = self.kernel
-		K_ss = self.kernel.get_kernel(self.kernel(*params),Xpredict,Xpredict)
+		K_ss = self.kernel(Xpredict,Xpredict,*self.par)
 		if not self.trained:
 			L_ss = np.linalg.cholesky(K_ss + 0.00005*np.eye(Xpredict.shape[0]))
 			f_prior = np.dot(L_ss, np.random.normal(size=(Xpredict.shape[0],n)))
@@ -47,7 +65,7 @@ class GP():
 				raise ValueError('GP not trained; please use the add_training function to train it')
 			else:
 				Lt = self.L_trained
-			Ks = self.kernel.get_kernel(self.kernel(*par),self.Xtrain,Xpredict)
+			Ks = self.kernel(self.Xtrain,Xpredict,*self.par)
 			Ls = np.linalg.solve(Lt,Ks)	
 			mu=np.dot(Ls.T, np.linalg.solve(Lt,self.Ytrain)).reshape((Xpredict.shape[0],))
 			
@@ -63,36 +81,15 @@ class GP():
 				plt.gca().fill_between(Xpredict.flat, mu-2*stdv, mu+2*stdv, color="#dddddd")
 				plt.plot(Xpredict,mu,'r--',lw=2)
 				plt.show()
-	
+			
 			return f_post
 			
-	def lml(self,theta):
-				sig=1
-				kern = copy.deepcopy(self.kernel)
-				kern.params=np.array((theta,sig))
-				K =  kern.get_kernel(self.Xtrain,self.Xtrain)
-				L=np.linalg.cholesky(K + 1e-6*np.eye(self.Xtrain.shape[0]))
-				log_like=0.5*np.dot(np.linalg.solve(L,self.Ytrain).T,np.linalg.solve(L,self.Ytrain))-np.sum(np.log(np.diagonal(L)),axis=0)-(self.Xtrain.shape[0]/2)*np.log(2*np.pi)
-				return log_like
-        
-class Kernel():
-	def __init__(self,params,params_bounds=None):
-			self.params=params
-			self.params_bounds=params_bounds
-	def get_par(self):
-		return self.params
-	def get_bounds(self):
-		return self.params_bounds
-	def theta(self):
-			return self.params[0]
-				
-class sqexp(Kernel):
-	def __init__(self,params,params_bounds=None):
-			self.params_list=['sig','len']
-	def get_kernel(self,a,b=None):
-		if b is None:
-			sqdist = pdist(a/self.params[0],metric='sqeuclidean')
-			returnself.params[1]*squareform(np.exp(-.5*sqdist))
-		else:
-			sqdist=cdist(a/self.params[0],b/self.params[0],metric='sqeuclidean')
-			return params[1]**np.exp(-0.5 * sqdist)
+def sqexp(a,b=None,l=0.5,sig=1):
+	if b is None:
+		sqdist = pdist(a/l,metric='sqeuclidean')
+		return sig*squareform(np.exp(-.5*sqdist))
+	else:
+		sqdist=cdist(a/l,b/l,metric='sqeuclidean')
+		return sig*np.exp(-0.5 * sqdist)
+
+	
